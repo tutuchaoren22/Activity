6fb201040d8bfce82f52f2143cb391e9866856d8
duyuqian <809009017@qq.com>
2020-08-13 22:01:32 +0800

    [duyuqian] feat: choose the contact when click the button

--- app/src/main/java/com/duyuqian/activity/ContactActivity.java
+++ app/src/main/java/com/duyuqian/activity/ContactActivity.java
@@ -1,14 +1,61 @@
 package com.duyuqian.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
+
+    static final int REQUEST_SELECT_CONTACT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        Button chooseContact = findViewById(R.id.choose_contact);
+        chooseContact.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selectContact();
+                    }
+                }
+        );
+    }
+
+
+    public void selectContact() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_CONTACT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK) {
+            assert data != null;
+            Uri contractUri = data.getData();
+            assert contractUri != null;
+            Cursor phoneCursor = getContentResolver().query(contractUri, null, null, null, null);
+            assert phoneCursor != null;
+            if (phoneCursor.moveToFirst()){
+                String contactName = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                phoneCursor.close();
+                TextView contactInfo = findViewById(R.id.contact_info);
+                contactInfo.setText(contactName.concat(phoneNumber) );
+            }
+        }
     }
 }
