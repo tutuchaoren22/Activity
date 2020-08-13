d16523c6656714ed7d225561a78f0e87e80500fe
duyuqian <809009017@qq.com>
2020-08-13 22:21:10 +0800

    [duyuqian] feat: print the lifecycle info in log

--- app/src/main/java/com/duyuqian/activity/LifeCycleActivity.java
+++ app/src/main/java/com/duyuqian/activity/LifeCycleActivity.java
@@ -3,6 +3,7 @@ package com.duyuqian.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class LifeCycleActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class LifeCycleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_cycle);
+        Log.i("Activity生命周期","调用onCreate()方法");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Activity生命周期","调用onStart()方法");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Activity生命周期","调用onResume()方法");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Activity生命周期","调用onPause()方法");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Activity生命周期","调用onStop()方法");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Activity生命周期","调用onRestart()方法");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Activity生命周期","调用onDestroy()方法");
     }
 }
